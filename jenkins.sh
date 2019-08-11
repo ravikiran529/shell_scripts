b704ac83140acd8a8bef0dc7034aefbe2d2e0423
Ravikiran <rrkiran@cloudzindagi.tk>
2019-08-11 15:44:15 +0000

    jenkins-commit

--- jenkins.sh
+++ jenkins.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
+set -e
+set -u
+#This script copies artifact to s3 bucket which has been built by maven.
 mkdir -p /root/scripts/jtest
 cd /root/scripts/jtest
 git clone https://github.com/ravikiran529/jenkinsRepo.git
 
-echo "Git repository JenkinsRepo cloned..."
+echo "____ Git repository JenkinsRepo cloned successfully. ____"
 
 cd jenkinsRepo/shivasai-app
 
-mvn clean deploy
-aws s3 cp /root/scripts/j2test/jenkinsRepo/shivasai-app/target/shivasai-app-1.0-SNAPSHOT.jar s3://cloudzindagi.tk/
-if (( $? == 0 )); then
-   echo "###########################################################################"     
+mvn clean package
+
+if [ "$?" != "0" ]
+then
+   echo "******** Build failed! ********"
+   exit 1
+fi
+
+PATH=/root/scripts/j2test/jenkinsRepo/shivasai-app/target/shivasai-app-1.0-SNAPSHOT.jar
+BUCKET=s3://cloudzindagi.tk/
+
+aws s3 cp $PATH $BUCKET
+
+if [ "$?" == "0" ]     
 
    echo "deploying artifact to s3 bucket is successful!"
 
-   echo "###########################################################################"
 else
-   echo "============================================================================"
+   
+   echo "Copying artifact to S3 bucket failed."
 
-   echo "Sorry! Not able to process your request at the moment."
-
-   echo "============================================================================"
 fi
 
 
